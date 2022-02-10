4f392890300baa3654cd29664b7b7b765a6de23d
Maky <franklinjaradev@gmail.com>
2022-02-09 19:06:04 -0500

    chore: adding autopep8 for python formatting

--- lua/lsp/null-ls.lua
+++ lua/lsp/null-ls.lua
@@ -14,6 +14,7 @@ null_ls.setup({
 		formatting.prettier,
 		formatting.black.with({ extra_args = { "--fast" } }),
 		formatting.eslint_d,
+    formatting.autopep8,
 		-- formatting.yapf,
 		formatting.stylua,
 		diagnostics.flake8,
