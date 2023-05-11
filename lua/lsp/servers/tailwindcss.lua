aedb715af549cccf3a8c6566e489d874f7f91054
Franklin Jara <franklinjaradev@gmail.com>
2023-05-10 20:50:03 -0400

    :sparkles: feat: update and clean up

--- lua/lsp/servers/tailwindcss.lua
+++ lua/lsp/servers/tailwindcss.lua
@@ -1,28 +1,31 @@
 -- npm install -g @tailwindcss/language-server
 return {
-  require"lspconfig".tailwindcss.setup{
-  settings = {
-      tailwindCSS = {
-        lint = {
-          cssConflict = "warning",
-          invalidApply = "error",
-          invalidConfigPath = "error",
-          invalidScreen = "error",
-          invalidTailwindDirective = "error",
-          invalidVariant = "error",
-          recommendedVariantOrder = "warning"
-        },
-        experimental = {
-          classRegex = {
-            "tw`([^`]*)",
-            "tw=\"([^\"]*)",
-            "tw={\"([^\"}]*)",
-            "tw\\.\\w+`([^`]*)",
-            "tw\\(.*?\\)`([^`]*)"
-          }
-        },
-        validate = true
-      }
-    }
-  }
+	require("lspconfig").tailwindcss.setup({
+		settings = {
+			tailwindCSS = {
+				lint = {
+					cssConflict = "warning",
+					invalidApply = "error",
+					invalidConfigPath = "error",
+					invalidScreen = "error",
+					invalidTailwindDirective = "error",
+					invalidVariant = "error",
+					recommendedVariantOrder = "warning",
+				},
+				experimental = {
+					classRegex = {
+						"tw`([^`]*)",
+						'tw="([^"]*)',
+						'tw={"([^"}]*)',
+						"tw\\.\\w+`([^`]*)",
+						"tw\\(.*?\\)`([^`]*)",
+						{ "clsx\\(([^)]*)\\)", "(?:'|\"|`)([^']*)(?:'|\"|`)" },
+						{ "classnames\\(([^)]*)\\)", "'([^']*)'" },
+						{ "cva\\(([^)]*)\\)", "[\"'`]([^\"'`]*).*?[\"'`]" },
+					},
+				},
+				validate = true,
+			},
+		},
+	}),
 }
